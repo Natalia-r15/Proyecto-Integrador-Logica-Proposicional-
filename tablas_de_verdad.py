8158d636745e301b4cf811db031004f91829f3c8
Natalia-r15 <delcarmen.ramirez@iteso.mx>
2025-09-18 17:06:28 -0600

    Update tablas_de_verdad.py

--- tablas_de_verdad.py
+++ tablas_de_verdad.py
@@ -3,8 +3,8 @@ valor = input("Ingresa T para la respuesta en texto o V para valores de verdad: 
 
 while correcto2:
     if valor == "T" or valor == "t":
-        q=input("p: ")
-        p=input("q: ")
+        p=input("p: ")
+        q=input("q: ")
         conector_logico=(input("Ingresa el nombre de conector lógico que deseas (conjuncion, disyuncion, negacion, "
                                "condicional, bicondicional o XOR: "))
         correcto = True
@@ -21,9 +21,16 @@ while correcto2:
             elif conector_logico == "bicondicional" or conector_logico == "Bicondicional":
                 print("p ↔ q: " + str(p) + " si y sólo si " + str(q)+ ".")
                 correcto = False
-            elif conector_logico == "XOR":
+            elif conector_logico == "XOR" or conector_logico == "xor":
                 print("p ⊕ q: " + str(p) + " o " + str(q) +", pero no ambos al mismo tiempo.")
                 correcto = False
+            elif conector_logico == "negacion" or conector_logico == "Negacion":
+                correcto = False
+                variable = input("Ingrese la variable que desea negar (p o q): ")
+                if variable == "p" or variable == "P":
+                    print("¬p: no " + str(p))
+                elif variable == "q" or variable == "Q":
+                    print("¬q: no " + str(q))
             else:
                 print("La palabra no es válida, intente de nuevo.")
                 conector_logico=(input("Ingresa el nombre de conector lógico que deseas: "))
@@ -61,6 +68,19 @@ while correcto2:
                         print("p ∧ q: F")
                     else:
                         print("p ∧ q: F")
+            elif conector_logico == "negacion" or conector_logico == "Negacion":
+                correcto3 = False
+                variable2 = input("Ingrese la variable que desea negar (p o q): ")
+                if variable2 == "p" or variable2 == "P":
+                    if p == "v" or p == "V":
+                        print("¬p: F")
+                    elif p == "f" or p == "F":
+                        print("¬p: V")
+                elif variable2 == "q" or variable2 == "Q":
+                    if q == "v" or q == "V":
+                        print("¬q: F")
+                    elif q == "f" or q == "F":
+                        print("¬q: V")
             elif conector_logico == "disyuncion" or conector_logico == "Disyuncion":
                 correcto3 = False
                 if p == "v" or p == "V":
